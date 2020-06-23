f25f16d3cdd56e18cb501db2bf306cd4f1fbbfc5
kadir <kadirsuzen28@gmail.com>
2020-06-22 19:37:03 -0500

    upload

--- src/test/java/Slides.java
+++ src/test/java/Slides.java
@@ -6,6 +6,6 @@ public class Slides {
     public static void main(String[] args) {
         System.out.println("hello world ");
     }
-//>>>>>>> 0b65d7d16b0a9e832f48dc22a7b06dc9fcd23945
+//>>>>>>> 0b65d7d16b0a9e832f48dc22a7b06dc9fcd23945 fasdfasdfasdfasd
 
 }
